bfbf7a6009dec7ba8ad79744b73cb22e9286cf51
Thomas Deconinck <thomas.deconinck@colisweb.com>
2026-02-24 16:32:28 +0100

    init with board movement logic

---
+++ src/board.rs
@@ -0,0 +1,229 @@
+use ratatui::{
+    layout::{Constraint, Layout},
+    widgets::{Block, Paragraph, Widget},
+};
+
+type Cell = Option<i32>;
+type Row = [Cell; 4];
+
+#[derive(Default, Clone)]
+pub struct Board {
+    state: [Row; 4],
+}
+
+pub enum Movement {
+    Up,
+    Down,
+    Right,
+    Left,
+}
+
+impl Board {
+    pub fn set_cell(&mut self, cell_value: Cell, x: usize, y: usize) {
+        self.state[x][y] = cell_value;
+    }
+    pub fn move_board(&mut self, movement: Movement) {
+        for col_index in 0..4 {
+            let (mut destination_cursor, mut iterator_index): (usize, isize) = match movement {
+                Movement::Up | Movement::Left => (0, 0),
+                Movement::Down | Movement::Right => (3, 3),
+            };
+
+            while match movement {
+                Movement::Up | Movement::Left => iterator_index < 4,
+                Movement::Down | Movement::Right => iterator_index >= 0,
+            } {
+                if destination_cursor as isize != iterator_index {
+                    let (current_x, current_y) = match movement {
+                        Movement::Up | Movement::Down => (destination_cursor, col_index),
+                        Movement::Left | Movement::Right => (col_index, destination_cursor),
+                    };
+                    let (next_x, next_y) = match movement {
+                        Movement::Up | Movement::Down => (iterator_index as usize, col_index),
+                        Movement::Left | Movement::Right => (col_index, iterator_index as usize),
+                    };
+
+                    match (self.state[current_x][current_y], self.state[next_x][next_y]) {
+                        (None, Some(o)) => {
+                            self.set_cell(Some(o), current_x, current_y);
+                            self.set_cell(None, next_x, next_y);
+                        }
+                        (Some(d), Some(o)) => {
+                            if d == o {
+                                self.set_cell(Some(d + o), current_x, current_y);
+                                self.set_cell(None, next_x, next_y);
+
+                                match movement {
+                                    Movement::Up | Movement::Left => destination_cursor += 1,
+                                    Movement::Down | Movement::Right => destination_cursor -= 1,
+                                };
+                            } else {
+                                match movement {
+                                    Movement::Up | Movement::Left => destination_cursor += 1,
+                                    Movement::Down | Movement::Right => destination_cursor -= 1,
+                                };
+                                continue;
+                            }
+                        }
+                        _ => (),
+                    }
+                }
+                match movement {
+                    Movement::Up | Movement::Left => iterator_index += 1,
+                    Movement::Down | Movement::Right => iterator_index -= 1,
+                };
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_set_board() {
+        let mut board = Board::default();
+        board.set_cell(Some(1), 1, 0);
+
+        assert_eq!(board.state[1][0], Some(1));
+    }
+
+    #[test]
+    fn test_board_move_down() {
+        let mut board = Board::default();
+        board.set_cell(Some(1), 1, 0);
+        assert_eq!(board.state[1][0], Some(1));
+        board.move_board(Movement::Down);
+
+        assert_eq!(board.state[0][0], None);
+        assert_eq!(board.state[1][0], None);
+        assert_eq!(board.state[2][0], None);
+        assert_eq!(board.state[3][0], Some(1));
+
+        let mut board2 = Board::default();
+        board2.set_cell(Some(2), 0, 0);
+        board2.set_cell(Some(2), 1, 0);
+        board2.set_cell(Some(2), 2, 0);
+        board2.set_cell(Some(2), 3, 0);
+        assert_eq!(board2.state[1][0], Some(2));
+        board2.move_board(Movement::Down);
+        assert_eq!(board2.state[0][0], None, "first row should be empty");
+        assert_eq!(board2.state[1][0], None, "second row should be empty");
+        assert_eq!(board2.state[2][0], Some(4), "third row should equal 4");
+        assert_eq!(board2.state[3][0], Some(4), "fourth row should equal 4");
+
+        let mut board3 = Board::default();
+        board3.set_cell(None, 0, 0);
+        board3.set_cell(Some(2), 1, 0);
+        board3.set_cell(Some(4), 2, 0);
+        board3.set_cell(Some(6), 3, 0);
+        board3.move_board(Movement::Up);
+        assert_eq!(board3.state[0][0], Some(2), "first row should equal 2");
+        assert_eq!(board3.state[1][0], Some(4), "second row should equal 4");
+        assert_eq!(board3.state[2][0], Some(6), "third row should equal 6");
+        assert_eq!(board3.state[3][0], None, "fourth row should be empty");
+    }
+
+    #[test]
+    fn test_board_move_up() {
+        let mut board = Board::default();
+        board.set_cell(Some(2), 0, 0);
+        board.set_cell(Some(2), 1, 0);
+        board.set_cell(Some(2), 2, 0);
+        board.set_cell(Some(2), 3, 0);
+        assert_eq!(board.state[1][0], Some(2));
+        board.move_board(Movement::Up);
+        assert_eq!(board.state[0][0], Some(4), "first row should equal 4");
+        assert_eq!(board.state[1][0], Some(4), "second row should equal 4");
+        assert_eq!(board.state[2][0], None, "third row should be empty");
+        assert_eq!(board.state[3][0], None, "fourth row should be empty");
+
+        // Impossible
+        let mut board3 = Board::default();
+        board3.set_cell(None, 0, 0);
+        board3.set_cell(Some(2), 1, 0);
+        board3.set_cell(Some(4), 2, 0);
+        board3.set_cell(None, 3, 0);
+        board3.move_board(Movement::Up);
+        assert_eq!(board3.state[0][0], Some(2), "first row should equal 2");
+        assert_eq!(board3.state[1][0], Some(4), "second row should equal 4");
+        assert_eq!(board3.state[2][0], None, "third row should be empty");
+        assert_eq!(board3.state[3][0], None, "fourth row should be empty");
+    }
+
+    #[test]
+    fn test_board_move_right() {
+        let mut board = Board::default();
+        board.set_cell(Some(2), 0, 0);
+        board.set_cell(Some(2), 0, 1);
+        board.set_cell(Some(2), 0, 2);
+        board.set_cell(Some(2), 0, 3);
+        assert_eq!(board.state[0][1], Some(2));
+        board.move_board(Movement::Right);
+        assert_eq!(board.state[0][0], None, "first column should be empty");
+        assert_eq!(board.state[0][1], None, "second column should be empty");
+        assert_eq!(board.state[0][2], Some(4), "third column should equal 4");
+        assert_eq!(board.state[0][3], Some(4), "fourth column should equal 4");
+
+        // Impossible
+        let mut board3 = Board::default();
+        board3.set_cell(None, 0, 0);
+        board3.set_cell(Some(2), 0, 1);
+        board3.set_cell(Some(4), 0, 2);
+        board3.set_cell(None, 0, 3);
+        board3.move_board(Movement::Right);
+        assert_eq!(board3.state[0][0], None, "first column should be empty");
+        assert_eq!(board3.state[0][1], None, "second column should be empty");
+        assert_eq!(board3.state[0][2], Some(2), "third column should equal 2");
+        assert_eq!(board3.state[0][3], Some(4), "fourth column should equal 4");
+    }
+
+    #[test]
+    fn test_board_move_left() {
+        let mut board = Board::default();
+        board.set_cell(Some(2), 0, 0);
+        board.set_cell(Some(2), 0, 1);
+        board.set_cell(Some(2), 0, 2);
+        board.set_cell(Some(2), 0, 3);
+        assert_eq!(board.state[0][1], Some(2));
+        board.move_board(Movement::Left);
+        assert_eq!(board.state[0][0], Some(4), "first column should equal 4");
+        assert_eq!(board.state[0][1], Some(4), "second column should equal 4");
+        assert_eq!(board.state[0][2], None, "third column should be empty");
+        assert_eq!(board.state[0][3], None, "fourth column should be empty");
+
+        // Impossible
+        let mut board3 = Board::default();
+        board3.set_cell(None, 0, 0);
+        board3.set_cell(Some(2), 0, 1);
+        board3.set_cell(Some(4), 0, 2);
+        board3.set_cell(None, 0, 3);
+        board3.move_board(Movement::Left);
+        assert_eq!(board3.state[0][0], Some(2), "first column should equal 2");
+        assert_eq!(board3.state[0][1], Some(4), "second column should equal 4");
+        assert_eq!(board3.state[0][2], None, "third column should be empty");
+        assert_eq!(board3.state[0][3], None, "fourth column should be empty");
+    }
+}
+
+impl Widget for Board {
+    fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer)
+    where
+        Self: Sized,
+    {
+        let col_constraints = (0..4).map(|_| Constraint::Length(8));
+        let row_constraints = (0..4).map(|_| Constraint::Length(4));
+        let horizontal = Layout::horizontal(col_constraints).spacing(1);
+        let vertical = Layout::vertical(row_constraints).spacing(1);
+
+        let rows = vertical.split(area);
+        let cells = rows.iter().flat_map(|&row| horizontal.split(row).to_vec());
+
+        for (i, cell) in cells.enumerate() {
+            Paragraph::new(format!("{}", i))
+                .block(Block::bordered())
+                .render(cell, buf);
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,72 @@
+mod board;
+
+use crate::board::{Board, Movement};
+use crossterm::event::{self, Event, KeyCode};
+use ratatui::{
+    DefaultTerminal, Frame,
+    layout::{Constraint, Layout},
+};
+
+#[derive(Default)]
+struct App {
+    board: Board,
+    should_quit: bool,
+}
+
+fn main() -> color_eyre::Result<()> {
+    let mut app = App::default();
+    color_eyre::install()?;
+    ratatui::run(|t| app.run(t))?;
+    Ok(())
+}
+
+enum AppEvent {
+    MoveBoard(Movement),
+    Quit,
+}
+
+impl App {
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> std::io::Result<()> {
+        while !self.should_quit {
+            terminal.draw(|f| self.render(f))?;
+            let event = crossterm::event::read()?;
+
+            if let crossterm::event::Event::Key(key) = event {
+                let mut app_event = self.handle_key(key, event);
+
+                while app_event.is_some() {
+                    app_event = self.handle_event(app_event.unwrap());
+                }
+            }
+        }
+        Ok(())
+    }
+
+    pub fn render(&mut self, frame: &mut Frame) {
+        let layout = Layout::default()
+            .direction(ratatui::layout::Direction::Horizontal)
+            .constraints(vec![Constraint::Percentage(50), Constraint::Min(1)])
+            .split(frame.area());
+
+        frame.render_widget(self.board.clone(), layout[0]);
+    }
+
+    fn handle_key(&mut self, key: event::KeyEvent, _event: Event) -> Option<AppEvent> {
+        match key.code {
+            KeyCode::Char('q') => Some(AppEvent::Quit),
+            KeyCode::Char('j') => Some(AppEvent::MoveBoard(Movement::Down)),
+            KeyCode::Char('k') => Some(AppEvent::MoveBoard(Movement::Up)),
+            KeyCode::Char('h') => Some(AppEvent::MoveBoard(Movement::Left)),
+            KeyCode::Char('l') => Some(AppEvent::MoveBoard(Movement::Right)),
+            _ => None,
+        }
+    }
+
+    fn handle_event(&mut self, app_event: AppEvent) -> Option<AppEvent> {
+        match app_event {
+            AppEvent::Quit => self.should_quit = true,
+            AppEvent::MoveBoard(movement) => self.board.move_board(movement),
+        }
+        None
+    }
+}
